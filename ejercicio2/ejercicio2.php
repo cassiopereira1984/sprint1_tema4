a4c070c365143fb2fb9722bb772a5ed64319409e
cassiopereira1984 <cassiopereira1984@gmail.com>
2024-02-20 20:03:05 +0100

    test include

--- ejercicio2/ejercicio2.php
+++ ejercicio2/ejercicio2.php
@@ -2,7 +2,10 @@
 /*Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt.
  Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea de la 
  forma area().*/
-$triangulo = new Triangulo (10, 20);
+include "triangulo.php";
+include "rectangulo.php";
+
+ $triangulo = new Triangulo (10, 20);
 echo "La area de triangulo es: " . $triangulo->area();
 
 
